ac9bf427c932125f394e2ee224bc0606b8d1dc17
Ed Lewis <ed.lewis@gmail.com>
2017-03-20 14:41:14 -0700

    start posts new

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-    return <div>Index</div>;
-}
+class PostsIndex extends Component {
+
+    componentWillMount() {
+        this.props.fetchPosts();
+    }
+
+     render() {
+         return (
+             <div>List of posts</div>
+         );
+     }
+ }
+
+ export default connect(null, { fetchPosts })(PostsIndex);
---
+++ src/components/posts_new.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react';
+
+class PostsNew extends Component {
+
+    render(){
+        return (
+            <div>
+                Create form
+            </div>
+        )
+    }
+}
+
+export default PostsNew;
