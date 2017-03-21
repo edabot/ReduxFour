be9beb9f6ac2c814a00c4f9b9cfa34caa18399e5
Ed Lewis <ed.lewis@gmail.com>
2017-03-21 14:40:55 -0700

    add deleting of post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
 export const DELETE_POST = 'DELETE_POST';
-export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
 const API_KEY = '?key=edabot';
--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,12 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
 
 class PostShow extends Component {
+
+    static contextTypes = {
+        router: PropTypes.object
+    };
+
     componentWillMount() {
         this.props.fetchPost(this.props.params.id);
     }
+
+    deletePost() {
+        this.props.deletePost(this.props.params.id).then(() => {
+            this.context.router.push('/');
+        } );
+    }
+
     render() {
 
         if (!this.props.post) {
@@ -14,12 +26,15 @@ class PostShow extends Component {
                 <div>Loading...</div>
             )
         }
+
+
         return (
             <div>
                 <Link to="/">Back to index</Link>
                 <h3>{this.props.post.title} </h3>
                 <h6>Categories: {this.props.post.categories}</h6>
                 <p>{this.props.post.content}</p>
+                <div onClick={this.deletePost.bind(this)}>delete</div>
             </div>
         );
     }
@@ -29,4 +44,4 @@ function mapStateToProps(state) {
     return { post: state.posts.post };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostShow);
